7a931f5a9546704ff7354032e9be400f42999e78
root <root@localhost.localdomain>
2026-07-07 18:09:42 +0800

    update: new data 07/07 v2

--- gen_csv.py
+++ gen_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime
 
 # 读取原始数据
-df = pd.read_excel('downloads/19f3ac99-9152-81e2-8000-0000a8dd94a3_list_20260707.xlsx', header=3)
+df = pd.read_excel('/root/.openclaw/workspace/downloads/19f3c0d3-0ee2-835b-8000-0000788b0e32_list_20260707_v2.xlsx', header=3)
 
 # 去掉空行和来源行
 df = df[df['CL1'].notna() & (df['CL1'] != 'Source: Capital IQ')].copy()
